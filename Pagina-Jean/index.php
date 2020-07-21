1120e2f3264f6b0acaff69295b08e191efc87a66
Jean Paul Espinosa <63934182+Jean-Espinosa@users.noreply.github.com>
2020-07-20 23:08:51 -0500

    Create index.php

---
+++ Pagina-Jean/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Inicio</title>
+	
+	<script src="https://kit.fontawesome.com/a076d05399.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+	<div class="header">
+		<nav class="navbar navbar-inverse">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>                        
+		      </button>
+		      <a class="navbar-brand emp">Transporte Público</a>
+		    </div>
+		    <div class="collapse navbar-collapse" id="myNavbar">
+		      <ul class="nav navbar-nav">
+		        <li class="active"><a href="index.php"><i class="fas fa-home"></i> Inicio</a></li>
+		        
+		        <li><a href="#"><i class="fas fa-user-circle"></i> Empleados</a></li>
+		        <li><a href="#"><i class="fas fa-users"></i> Usuarios</a></li>
+		        <li><a href="#"><i class="fas fa-clipboard"></i> Contratos</a></li>
+		        <li><a href="#"><i class="fas fa-bus"></i> Buses</a></li>
+		        <li><a href="#"><i class="fas fa-road"></i> Rutas</a></li>
+		      </ul>
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Cerrar Sesión</a></li>
+		      </ul>
+		    </div>
+		  </div>
+		</nav>
+	</div>
+
+	<center>
+		<h1>Bienvenido</h1>
+	</center>
+</body>
+</html>
